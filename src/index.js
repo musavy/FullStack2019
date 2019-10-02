1deb54cbc1df4812f1e2be4916279bd6d9511502
musavy <sayed.musavy@integrify.io>
2019-10-02 22:18:30 +0300

    first part1 done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("p", null, "Hello world, it is", now.toString()),
-    React.createElement("p", null, a, "plus", b, "is", a + b)
+const Hello = props => {
+  return (
+    <div>
+      <p>
+        Hello{props.name}, you are {props.age} years old
+      </p>
+    </div>
   );
 };
 
-ReactDOM.render(
-  React.createElement(App, null),
-  document.getElementById("root")
-);
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by
+      <a href="https://github.com/mluukkai"> mluukkai </a>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = "Peter";
+  const age = 10;
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
